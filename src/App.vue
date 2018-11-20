854feb8bebd213e23c2f9d3759e803fc71ef4c59
Zu3zz <zthzththth@163.com>
2018-11-20 13:59:40 +0800

    add star

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,35 @@
 <template>
   <div id="app">
-    <v-header></v-header>
+    <v-header :seller="seller"></v-header>
+    <header-detail :seller="seller"></header-detail>
   </div>
 </template>
 
 <script>
   import VHeader from './components/v-header/v-header'
+  import HeaderDetail from './components/header-detail/header-detail'
+  import { getSeller } from 'api'
 
   export default {
     name: 'app',
+    data () {
+      return {
+        seller: {}
+      }
+    },
+    created () {
+      this._getSeller()
+    },
+    methods: {
+      _getSeller() {
+        getSeller().then((seller) => {
+          this.seller = seller
+        })
+      }
+    },
     components: {
-      VHeader
+      VHeader,
+      HeaderDetail
     }
   }
 </script>
